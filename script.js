05bc6eb7d707d78ae37a913bdc70289053fdd8b3
rizkosefta <seftaal89@gmail.com>
2023-05-08 17:23:48 +0700

    commit pertama

---
+++ script.js
@@ -0,0 +1,17 @@
+// typed js home
+
+const typed = new Typed(
+    '.mahasiswa-text', {
+    strings : ['Mahasiswa Teknik Informatika'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop : true});
+const type = new Typed(
+    '.keahlian-text', {
+    strings : ['HTML (70%)','CSS (70%)','Java Script (50%)','Pemrograman C (65%)',
+    'Pemrograman C++ (65%)','Pemrograman C# (65%)','Pemrograman Java (65%)'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 3000,
+    loop : true});
